cc4a39ab903038fd5c2b911910ae0381daa26a22
mexlataster <mex@Mexs-MacBook-Air.local>
2016-04-19 09:06:57 +0200

    Angels PHP opdracht

---
+++ pages/includes/header.php
@@ -0,0 +1,4 @@
+<div class="header">
+	<h1><?php echo $h1 ?></h1>
+	<p class="subtitle"><?php echo $p ?></p>
+</div>
---
+++ pages/includes/menu.php
@@ -0,0 +1,6 @@
+<ul class="menu">
+	<li><a href="<?php echo $l1; ?>" class="<?php echo $la1 ?>">Home</a></li>
+	<li><a href="<?php echo $l2; ?>" class="<?php echo $la2 ?>">The High Heavens</a></li>
+	<li><a href="<?php echo $l3; ?>" class="<?php echo $la3 ?>">Guardian Angel</a></li>
+	<li><a href="<?php echo $l4; ?>" class="<?php echo $la4 ?>">Our Evolution</a></li>
+</ul>
